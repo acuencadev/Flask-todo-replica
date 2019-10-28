d9a2c0752b580f2e8611bdb435a7af276a92093a
Amador Cuenca <sphi02ac@gmail.com>
2019-10-28 14:42:49 -0300

    Import the Bootstrap template

--- todo_replica/routes/main.py
+++ todo_replica/routes/main.py
@@ -1,8 +1,9 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 
 main = Blueprint('main', __name__)
 
+
 @main.route('/')
 def index():
-    return 'Hello World!'
+    return render_template('main/index.html')
