ae4637a6523119732209789044e1b6f4a9b58351
Amador Cuenca <sphi02ac@gmail.com>
2019-10-28 14:48:25 -0300

    Initialize the SQLAlchemy extension

--- todo_replica/__init__.py
+++ todo_replica/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 
 from .routes.main import main
+from .extensions import db
 
 
 def create_app(config_file='settings.py'):
@@ -8,7 +9,8 @@ def create_app(config_file='settings.py'):
     
     app.config.from_pyfile(config_file)
     
-    # TODO: Register the extensions
+    # Register the extensions
+    db.init_app(app)
     
     # Register the blueprints
     app.register_blueprint(main)
