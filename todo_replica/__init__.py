2df846ee6f5ef286bc947300ba0bd97bd8009a62
Amador Cuenca <sphi02ac@gmail.com>
2019-10-28 14:25:08 -0300

    Create the main blueprint

--- todo_replica/__init__.py
+++ todo_replica/__init__.py
@@ -1,7 +1,16 @@
 from flask import Flask
 
+from .routes.main import main
 
-def create_app():
+
+def create_app(config_file='settings.py'):
     app = Flask(__name__)
+    
+    app.config.from_pyfile(config_file)
+    
+    # TODO: Register the extensions
+    
+    # Register the blueprints
+    app.register_blueprint(main)
     
     return app
