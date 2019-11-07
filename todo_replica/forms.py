cb48c8533564ed7403a1173474e596193786c904
Amador Cuenca <sphi02ac@gmail.com>
2019-11-07 17:30:25 -0300

    Add the submit button to the forms

--- todo_replica/forms.py
+++ todo_replica/forms.py
@@ -1,22 +1,22 @@
 from flask_wtf import FlaskForm
-from wtforms import TextField, PasswordField
+from wtforms import TextField, PasswordField, SubmitField
 from wtforms.validators import Required, Length
 
 
 class LoginForm(FlaskForm):
-    username = TextField('username', validators=[
+    username = TextField('Username', validators=[
         Length(min=5, max=50),
         Required()])
-    password = PasswordField('password', validators=[
+    password = PasswordField('Password', validators=[
         Length(min=6, max=100),
         Required()])
-    ])
+    submit = SubmitField('Login')
 
 class RegisterForm(FlaskForm):
-    username = TextField('username', validators=[
+    username = TextField('Username', validators=[
         Length(min=5, max=50),
         Required()])
-    password = PasswordField('password', validators=[
+    password = PasswordField('Password', validators=[
         Length(min=6, max=100),
         Required()])
-    ])
+    submit = SubmitField('Register')
