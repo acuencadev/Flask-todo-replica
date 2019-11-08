c3422207809b51941b00f46d9fd0309a8e91cd02
Amador Cuenca <sphi02ac@gmail.com>
2019-11-08 14:48:33 -0300

    Add confirm field to the register form

--- todo_replica/forms.py
+++ todo_replica/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import TextField, PasswordField, SubmitField
-from wtforms.validators import Required, Length
+from wtforms.validators import Required, Length, EqualTo
 
 
 class LoginForm(FlaskForm):
@@ -12,11 +12,16 @@ class LoginForm(FlaskForm):
         Required()])
     submit = SubmitField('Login')
 
+
 class RegisterForm(FlaskForm):
     username = TextField('Username', validators=[
         Length(min=5, max=50),
         Required()])
     password = PasswordField('Password', validators=[
+        Length(min=6, max=100),
+        Required(),
+        EqualTo('confirm_password', message="Passwords must match.")])
+    confirm_password = PasswordField('Password', validators=[
         Length(min=6, max=100),
         Required()])
     submit = SubmitField('Register')
