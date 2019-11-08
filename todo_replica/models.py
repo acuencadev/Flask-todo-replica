6ba629eb344b3b09683295d96974fb91d4fa8a34
Amador Cuenca <sphi02ac@gmail.com>
2019-11-08 15:01:01 -0300

    Encrypt the password using Bcrypt

--- todo_replica/models.py
+++ todo_replica/models.py
@@ -1,13 +1,14 @@
 import datetime
 from flask_login import UserMixin
+from sqlalchemy.ext.hybrid import hybrid_property
 
-from .extensions import db
+from .extensions import db, bcrypt
 
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50))
-    password = db.Column(db.String(100))
+    _password = db.Column(db.String(100))
     active = db.Column(db.Boolean())
     created_at = db.Column(db.DateTime(), default=datetime.datetime.utcnow)
     last_updated_at = db.Column(db.DateTime(), default=datetime.datetime.utcnow)
@@ -17,6 +18,17 @@ class User(db.Model, UserMixin):
         foreign_keys='Task.user_id',
         backref='owner',
         lazy=True)
+    
+    @hybrid_property
+    def password(self):
+        return self._password
+    
+    @password.setter
+    def _set_password(self, plain_pass):
+        self._password = bcrypt.generate_password_hash(plain_pass)
+        
+    def is_correct_password(self, plain_pass):
+        return bcrypt.check_password_hash(self._password, plain_pass)
 
 
 class Task(db.Model):
