57aca8dd2cb8870ca00029b573d31e9ab8212814
Amador Cuenca <sphi02ac@gmail.com>
2019-11-15 14:34:50 -0300

    Create the new task route

--- todo_replica/routes/main.py
+++ todo_replica/routes/main.py
@@ -1,7 +1,9 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for
 from flask_login import login_required, current_user
 
+from todo_replica.extensions import db
 from todo_replica.models import Task
+from todo_replica.forms import TaskForm
 
 
 main = Blueprint('main', __name__)
@@ -13,3 +15,19 @@ def index():
     tasks = Task.query.filter_by(user_id=current_user.get_id()).all()
     
     return render_template('main/index.html', tasks=tasks)
+
+
+@main.route('/task/new', methods=['GET', 'POST'])
+@login_required
+def create_task():
+    form = TaskForm()
+    
+    if form.validate_on_submit():
+        task = Task(description=form.description.data)
+        
+        db.session.add(task)
+        db.session.commit()
+    
+        return redirect(url_for('main.index'))
+    
+    return render_template('main/create.html')
