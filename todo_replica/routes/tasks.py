152607604c7ba2136af86859e3a675eff3841f3d
Amador Cuenca <sphi02ac@gmail.com>
2019-11-18 13:05:47 -0300

    Rename the pending task route

--- todo_replica/routes/tasks.py
+++ todo_replica/routes/tasks.py
@@ -11,10 +11,10 @@ tasks = Blueprint('tasks', __name__)
 
 @tasks.route('/')
 @login_required
-def uncompleted():
+def pending():
     tasks = Task.query.filter_by(user_id=current_user.get_id(), completed=False).all()
     
-    return render_template('tasks/index.html', tasks=tasks)
+    return render_template('tasks/pending.html', tasks=tasks)
 
 
 @tasks.route('/done')
@@ -36,6 +36,6 @@ def create():
         db.session.add(task)
         db.session.commit()
     
-        return redirect(url_for('tasks.uncompleted'))
+        return redirect(url_for('tasks.pending'))
     
     return render_template('tasks/create.html', form=form)
